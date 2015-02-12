7aab7c076ff0fe21b1eaf6fd2a984f387bdb058f
huangmingyou <huangmingyou@gmail.com>
2015-02-12 11:05:30 +0800

    update

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 basedir=`pwd`
+[ "$1" == "" ]&&{
+echo "Useage: $0 <[classname1|,classname2]>"
+exit 0
+}
 puppet apply --ordering manifest --modulepath=${basedir}/modules ${basedir}/manifests/site.pp -e "include $1"
