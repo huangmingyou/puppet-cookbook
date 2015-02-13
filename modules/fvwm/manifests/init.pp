fc08a4a9fcbd37a81eba09a646d5920ccb428ec4
huangmingyou <huangmingyou@gmail.com>
2015-02-13 10:29:41 +0800

    誓将挂冠去，觉道资无穷。

--- modules/fvwm/manifests/init.pp
+++ modules/fvwm/manifests/init.pp
@@ -1,34 +1,28 @@
 class fvwm {
 package {
-        ["gtk-theme-switch","gtk2-engines-oxygen","ssh-askpass","wpasupplicant","acpi-support","keynav","fvwm","xfonts-terminus","xfonts-wqy","tmux","rxvt-unicode","fcitx-googlepinyin","ecryptfs-utils","p7zip-full","git-core","dmz-cursor-theme","gnupg-agent"]:
+        ["gtk-theme-switch","gtk2-engines-oxygen","ssh-askpass","wpasupplicant","acpi-support","keynav","fvwm","xfonts-terminus","xfonts-wqy","tmux","rxvt-unicode","fcitx-googlepinyin","encfs","p7zip-full","git-core","dmz-cursor-theme","gnupg-agent"]:
         ensure => installed;
 }
 
+File {
+	owner => $username,
+	group => $username,
+}
 
 file {
 	"/home/${username}/.fvwm2rc":
-	owner => $username,
-	group => $username,
 	source => "puppet:///modules/fvwm/fvwm2rc";
 
 	"/home/${username}/.Xresources":
-	owner => $username,
-	group => $username,
 	source => "puppet:///modules/fvwm/Xresources";
 
 	"/home/${username}/.tmux.conf":
-	owner => $username,
-	group => $username,
 	source => "puppet:///modules/fvwm/tmux.conf";
 
 	"/home/${username}/.fonts.conf":
-	owner => $username,
-	group => $username,
 	source => "puppet:///modules/fvwm/fonts.conf";
 
 	"/home/${username}/.bashrc":
-	owner => $username,
-	group => $username,
 	source => "puppet:///modules/fvwm/bashrc";
 
 	"/etc/bash_completion.d/bash_complete.s":
