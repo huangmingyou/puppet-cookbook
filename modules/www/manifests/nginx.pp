c67220ee115a76ce7ce085138efae271e5ce4e42
huangmingyou <huangmingyou@gmail.com>
2015-06-03 16:26:55 +0800

    add www

---
+++ modules/www/manifests/nginx.pp
@@ -0,0 +1,6 @@
+class www::nginx {
+	package {
+		["nginx-full","php5-fpm","php5","php5-cgi","php5-cli","php5-common","php5-curl","php5-gd","php5-imap","php5-mcrypt","php5-mysql","php5-snmp","subversion"]:
+		ensure => installed;
+	}
+}
