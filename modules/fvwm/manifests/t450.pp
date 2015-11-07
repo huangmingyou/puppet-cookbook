a2e8f7e925d799f7f8c1998d18fa119c659dd6b3
hmy <hmy@debian>
2015-11-07 21:17:38 +0800

    add t450

--- modules/fvwm/manifests/t450.pp
+++ modules/fvwm/manifests/t450.pp
@@ -1,6 +1,22 @@
 class fvwm::t450 {
+$username="hmy"
+
+
 	file {
 		"/etc/systemd/system/slock.service":
 		source => puppet:///modules/fvwm/slock.service";
+		"/home/${username}/.asoundrc":
+		content => "defaults.pcm.card=0\n";
+		"/etc/modprobe.d/thinkpad-t450s.conf":
+		content => "options snd_hda_intel index=1,0\n";
 	}
+	exec {
+		"enable slock":
+		exec => "systemctl enable slock.service";
+	}
+	package {
+		"firmware-iwlwifi":
+		ensure => installed;
+	}
+	
 }
